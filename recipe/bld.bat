9ca3b99287495fd659bccea8241c0b8543670513
giumas <gmasetti@ccom.unh.edu>
2018-03-14 09:44:08 -0400

    changes to install the import and static libs under the 'Library\lib' folder

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,8 +10,8 @@ mkdir build_static_mt
 cd build_static_mt
 cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release %GTEST_DIR%
 nmake
-copy gtest.lib %LIBRARY_BIN%
-copy gtest_main.lib %LIBRARY_BIN%
+copy gtest.lib %LIBRARY_LIB%
+copy gtest_main.lib %LIBRARY_LIB%
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
 
@@ -20,8 +20,8 @@ mkdir build_static_mtd
 cd build_static_mtd
 cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Debug %GTEST_DIR%
 nmake
-copy gtest.lib %LIBRARY_BIN%\gtestd.lib
-copy gtest_main.lib %LIBRARY_BIN%\gtest_maind.lib
+copy gtest.lib %LIBRARY_LIB%\gtestd.lib
+copy gtest_main.lib %LIBRARY_LIB%\gtest_maind.lib
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
 
@@ -30,8 +30,8 @@ mkdir build_static_md
 cd build_static_md
 cmake -G "NMake Makefiles" -D gtest_force_shared_crt=ON -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release %GTEST_DIR%
 nmake
-copy gtest.lib %LIBRARY_BIN%\gtest-md.lib
-copy gtest_main.lib %LIBRARY_BIN%\gtest_main-md.lib
+copy gtest.lib %LIBRARY_LIB%\gtest-md.lib
+copy gtest_main.lib %LIBRARY_LIB%\gtest_main-md.lib
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
 
@@ -40,8 +40,8 @@ mkdir build_static_mdd
 cd build_static_mdd
 cmake -G "NMake Makefiles" -D gtest_force_shared_crt=ON -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Debug %GTEST_DIR%
 nmake
-copy gtest.lib %LIBRARY_BIN%\gtest-mdd.lib
-copy gtest_main.lib %LIBRARY_BIN%\gtest_main-mdd.lib
+copy gtest.lib %LIBRARY_LIB%\gtest-mdd.lib
+copy gtest_main.lib %LIBRARY_LIB%\gtest_main-mdd.lib
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
 
@@ -51,7 +51,7 @@ cd build_dynamic_mt
 cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release -D gtest_build_tests=ON %GTEST_DIR%
 nmake gtest_dll
 copy gtest_dll.dll %LIBRARY_BIN%
-copy gtest_dll.lib %LIBRARY_BIN%
+copy gtest_dll.lib %LIBRARY_LIB%
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
 
@@ -61,7 +61,7 @@ cd build_dynamic_mtd
 cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Debug -D gtest_build_tests=ON %GTEST_DIR%
 nmake gtest_dll
 copy gtest_dll.dll %LIBRARY_BIN%\gtest_dlld.dll
-copy gtest_dll.lib %LIBRARY_BIN%\gtest_dlld.lib
+copy gtest_dll.lib %LIBRARY_LIB%\gtest_dlld.lib
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
 
@@ -71,7 +71,7 @@ cd build_dynamic_md
 cmake -G "NMake Makefiles" -D gtest_force_shared_crt=ON -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release -D gtest_build_tests=ON %GTEST_DIR%
 nmake gtest_dll
 copy gtest_dll.dll %LIBRARY_BIN%\gtest_dll-md.dll
-copy gtest_dll.lib %LIBRARY_BIN%\gtest_dll-md.lib
+copy gtest_dll.lib %LIBRARY_LIB%\gtest_dll-md.lib
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
 
@@ -81,6 +81,6 @@ cd build_dynamic_mdd
 cmake -G "NMake Makefiles" -D gtest_force_shared_crt=ON -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Debug -D gtest_build_tests=ON %GTEST_DIR%
 nmake gtest_dll
 copy gtest_dll.dll %LIBRARY_BIN%\gtest_dll-mdd.dll
-copy gtest_dll.lib %LIBRARY_BIN%\gtest_dll-mdd.lib
+copy gtest_dll.lib %LIBRARY_LIB%\gtest_dll-mdd.lib
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
