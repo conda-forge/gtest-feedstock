7e437aa7c725a6ce973b3b535b009160b3f35bbd
trevisan <marcelo.trevisani@xfel.eu>
2018-06-04 17:01:25 +0200

    Removed dependency of nmake in windows platform and small improvements

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,79 +8,80 @@ cd %GTEST_DIR%
 REM Build and copy static libraries (Release)
 mkdir build_static_mt
 cd build_static_mt
-cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release %GTEST_DIR%
-nmake
-copy gtest.lib %LIBRARY_LIB%
-copy gtest_main.lib %LIBRARY_LIB%
+cmake -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" %GTEST_DIR%
+cmake --build . --target ALL_BUILD --config Release
+copy Release\gtest.lib %LIBRARY_LIB%
+copy Release\gtest_main.lib %LIBRARY_LIB%
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
 
 REM Build and copy static libraries (Debug)
 mkdir build_static_mtd
 cd build_static_mtd
-cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Debug %GTEST_DIR%
-nmake
-copy gtest.lib %LIBRARY_LIB%\gtestd.lib
-copy gtest_main.lib %LIBRARY_LIB%\gtest_maind.lib
+cmake -G "%CMAKE_GENERATOR%" -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" %GTEST_DIR%
+cmake --build . --target ALL_BUILD --config Debug
+copy Debug\gtest.lib %LIBRARY_LIB%\gtestd.lib
+copy Debug\gtest_main.lib %LIBRARY_LIB%\gtest_maind.lib
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
 
 REM Build and copy static libraries with shared run-time library (Release)
 mkdir build_static_md
 cd build_static_md
-cmake -G "NMake Makefiles" -D gtest_force_shared_crt=ON -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release %GTEST_DIR%
-nmake
-copy gtest.lib %LIBRARY_LIB%\gtest-md.lib
-copy gtest_main.lib %LIBRARY_LIB%\gtest_main-md.lib
+cmake -G "%CMAKE_GENERATOR%" -D gtest_force_shared_crt=ON -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" %GTEST_DIR%
+cmake --build . --target ALL_BUILD --config Release
+copy Release\gtest.lib %LIBRARY_LIB%\gtest-md.lib
+copy Release\gtest_main.lib %LIBRARY_LIB%\gtest_main-md.lib
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
 
 REM Build and copy static libraries with shared run-time library (Debug)
 mkdir build_static_mdd
 cd build_static_mdd
-cmake -G "NMake Makefiles" -D gtest_force_shared_crt=ON -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Debug %GTEST_DIR%
-nmake
-copy gtest.lib %LIBRARY_LIB%\gtest-mdd.lib
-copy gtest_main.lib %LIBRARY_LIB%\gtest_main-mdd.lib
+cmake -G "%CMAKE_GENERATOR%" -D gtest_force_shared_crt=ON -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" %GTEST_DIR%
+cmake --build . --target ALL_BUILD --config Debug
+copy Debug\gtest.lib %LIBRARY_LIB%\gtest-mdd.lib
+copy Debug\gtest_main.lib %LIBRARY_LIB%\gtest_main-mdd.lib
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
 
 REM Build and copy dynamic libraries (Release)
 mkdir build_dynamic_mt
 cd build_dynamic_mt
-cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release -D gtest_build_tests=ON %GTEST_DIR%
-nmake gtest_dll
-copy gtest_dll.dll %LIBRARY_BIN%
-copy gtest_dll.lib %LIBRARY_LIB%
+cmake -G "%CMAKE_GENERATOR%" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D gtest_build_tests=ON -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" %GTEST_DIR%
+cmake --build . --target gtest_dll --config Release
+copy Release\gtest_dll.dll %LIBRARY_BIN%
+copy Release\gtest_dll.lib %LIBRARY_LIB%
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
 
 REM Build and copy dynamic libraries (Debug)
 mkdir build_dynamic_mtd
 cd build_dynamic_mtd
-cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Debug -D gtest_build_tests=ON %GTEST_DIR%
-nmake gtest_dll
-copy gtest_dll.dll %LIBRARY_BIN%\gtest_dlld.dll
-copy gtest_dll.lib %LIBRARY_LIB%\gtest_dlld.lib
+cmake -G "%CMAKE_GENERATOR%" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D gtest_build_tests=ON -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" %GTEST_DIR%
+cmake --build . --target gtest_dll --config Debug
+copy Debug\gtest_dll.dll %LIBRARY_BIN%\gtest_dlld.dll
+copy Debug\gtest_dll.lib %LIBRARY_LIB%\gtest_dlld.lib
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
 
 REM Build and copy dynamic libraries with shared run-time library (Release)
 mkdir build_dynamic_md
 cd build_dynamic_md
-cmake -G "NMake Makefiles" -D gtest_force_shared_crt=ON -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release -D gtest_build_tests=ON %GTEST_DIR%
-nmake gtest_dll
-copy gtest_dll.dll %LIBRARY_BIN%\gtest_dll-md.dll
-copy gtest_dll.lib %LIBRARY_LIB%\gtest_dll-md.lib
+cmake -G "%CMAKE_GENERATOR%" -D gtest_force_shared_crt=ON -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D gtest_build_tests=ON -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" %GTEST_DIR%
+cmake --build . --target gtest_dll --config Release
+copy Release\gtest_dll.dll %LIBRARY_BIN%\gtest_dll-md.dll
+copy Release\gtest_dll.lib %LIBRARY_LIB%\gtest_dll-md.lib
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
 
 REM Build and copy dynamic libraries with shared run-time library (Debug)
 mkdir build_dynamic_mdd
 cd build_dynamic_mdd
-cmake -G "NMake Makefiles" -D gtest_force_shared_crt=ON -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Debug -D gtest_build_tests=ON %GTEST_DIR%
-nmake gtest_dll
-copy gtest_dll.dll %LIBRARY_BIN%\gtest_dll-mdd.dll
-copy gtest_dll.lib %LIBRARY_LIB%\gtest_dll-mdd.lib
+cmake -G "%CMAKE_GENERATOR%" -D gtest_force_shared_crt=ON -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D gtest_build_tests=ON -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" %GTEST_DIR%
+cmake --build . --target gtest_dll --config Debug
+copy Debug\gtest_dll.dll %LIBRARY_BIN%\gtest_dll-mdd.dll
+copy Debug\gtest_dll.lib %LIBRARY_LIB%\gtest_dll-mdd.lib
 if errorlevel 1 exit 1
 cd %GTEST_DIR%
+
