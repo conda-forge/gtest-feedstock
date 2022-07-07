08309bd54aa4857819272318485dbb2572437a4c
Michael C. Grant <mgrant@anaconda.com>
2022-07-07 13:03:20 -0500

    align with upstream

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-GTEST_DIR=$SRC_DIR/googletest
-
-# Copy headers
-cp -r $GTEST_DIR/include/gtest $PREFIX/include/
-
-# Build and copy static libraries
-mkdir build_static
-cd build_static
-cmake $GTEST_DIR
-make
-cp libgtest.a $PREFIX/lib/
-cp libgtest_main.a $PREFIX/lib/
-cd $GTEST_DIR
-
-# Build and copy dynamic libraries
-mkdir build_dynamic
-cd build_dynamic
-cmake $GTEST_DIR -Dgtest_build_tests=ON
-make -j${CPU_COUNT} ${VERBOSE_CM}
-cp libgtest_dll${SHLIB_EXT} $PREFIX/lib/
-cd $GTEST_DIR
+# Build and install dynamic library
+mkdir build
+cd build
+cmake ${CMAKE_ARGS} \
+  -GNinja \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DBUILD_SHARED_LIBS=ON \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DCMAKE_BUILD_TYPE=Release \
+  ..
+ninja install
